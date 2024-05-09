e045e19102273a12a5682f10457f326db3bc4300
Alexander A. Kapralov <alnkapa@gmail.com>
2024-05-09 19:56:30 +0300

    Исправил ошибку в условии.

--- main.cpp
+++ main.cpp
@@ -6,35 +6,30 @@
 
 int main() {
   auto sst{read(std::cin)};
-  bool enter = false;
   // выводим полный список адресов после сортировки. Одна строка - один
   // адрес.
   for (auto &num : sst) {
-    if (!enter) {
-      // первая строка
-      enter = true;
-    } else {
-      std::cout << "\n";
-    }
-    std::cout << num;
+    std::cout << num << "\n";
   }
   // выводим список адресов, первый байт которых равен 1.
   for (auto &num : sst) {
     if (num.m_num0 == 1) {
-      std::cout << "\n" << num;
+      std::cout << num << "\n";
     }
   }
   // выводим список адресов, первый байт которых равен 46, а второй 70.
   for (auto &num : sst) {
     if (num.m_num0 == 46 && num.m_num1 == 70) {
-      std::cout << "\n" << num;
+      std::cout << num << "\n";
+      ;
     }
   }
   // выводим список адресов, любой байт которых равен 46.
   for (auto &num : sst) {
-    if (num.m_num0 == 46 || num.m_num1 == 46 && num.m_num2 == 46 ||
+    if (num.m_num0 == 46 || num.m_num1 == 46 || num.m_num2 == 46 ||
         num.m_num3 == 46) {
-      std::cout << "\n" << num;
+      std::cout << num << "\n";
+      ;
     }
   }
   return 0;
